61f91e7357304f31b2f1e65c5ff4564bb2a00e44
Ramo <ramo160689@gmail.com>
2023-07-25 16:57:17 +0200

    added sign macro to utils

--- src/utils.h
+++ src/utils.h
@@ -1,33 +1,12 @@
 #pragma once
 #include "pd_api.h"
 #include <stdbool.h>
+#include <math.h>
 
 #define EPSILON 0.0001
+#define sign(x)  (signbit(x) ?  -1 : 1)
 
-inline bool AreSame(float a, float b)
-{
-    return fabs(a - b) < EPSILON;
-}
+bool AreSame(float a, float b);
 
 // Function to find the middle of three number
-float middleOfThree(float a, float b, float c)
-{
-    // x is positive if a is greater than b.
-    // x is negative if b is greater than a.
-    float x = a - b;
-
-    float y = b - c;  // Similar to x
-    float z = a - c;  // Similar to x and y.
-
-    // Checking if b is middle (x and y both
-    // are positive)
-    if (x * y > 0)
-        return b;
-
-    // Checking if c is middle (x and z both
-    // are positive)
-    else if (x * z > 0)
-        return c;
-    else
-        return a;
-}
+float middleOfThree(float a, float b, float c);
