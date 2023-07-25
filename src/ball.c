ce805e9a36adc971fef29421eef0bd364c5c40ec
Ramo <ramo160689@gmail.com>
2023-07-25 16:49:06 +0200

    change ball angle on collision with paddle

--- src/ball.c
+++ src/ball.c
@@ -1,15 +1,21 @@
 #include "ball.h"
 #include "brick.h"
+#include "paddle.h"
 #include "memory.h"
 #include "common.h"
+#include "utils.h"
+
+#include <stdlib.h>
 
 struct BallData
 {
 	float dx;
 	float dy;
 	float speed;
+	float angle;
 	bool stuck;
 	bool dead;
+	bool rammed;
 };
 
 LCDSprite* ball_create(float x, float y, LCDBitmap* image)
@@ -40,8 +46,10 @@ LCDSprite* ball_create(float x, float y, LCDBitmap* image)
 	ball_data->dx = BALL_DX;
 	ball_data->dy = BALL_DY;
 	ball_data->speed = BALL_SPEED;
+	ball_data->angle = BALL_ANGLE;
 	ball_data->stuck = false;
 	ball_data->dead = false;
+	ball_data->rammed = false;
 	pd->sprite->setUserdata(ball, (void*)ball_data);
 	return ball;
 }
@@ -108,7 +116,63 @@ void ball_update_sprite(LCDSprite* sprite)
 
 				if (other_tag == PADDLE) 
 				{
+					float pad_x = 0;
+                                        float pad_y = 0;
+                                        pd->sprite->getPosition(collisions[0].other, &pad_x, &pad_y);
+ 
+                                        bool bend = false;
+                                        bool angf = false;
+                                        if (collisions[0].normal.x != 0)
+                                        {
+                                                if (actual_y + ball_rect.width > pad_y + 3)
+                                                {
+                                                        ball_data->rammed = true;
+                                                }
+                                                else
+						{
+                                                        bend = true;
+                                                        angf = false;
+                                                        collisions[0].normal.y = -1;
+                                                        pd->sprite->moveTo(sprite, nextx, pad_y - ball_rect.height);
+                                                }
+                                        }
+
 					ball_reflect(ball_data, collisions[0].normal);
+
+					if (collisions[0].normal.y == -1)
+                                        {
+                                                if (!bend && abs(paddle_get_dx(collisions[0].other)) > 1)
+                                                {
+                                                        bend = true;
+                                                        if (sign(paddle_get_dx(collisions[0].other)) == sign(ball_get_dx(sprite)))
+                                                        {
+                                                                angf = true;
+                                                        }
+                                                        else
+                                                        {
+                                                                angf = false;
+                                                        }
+                                                }
+						
+						if (bend)
+                                                {
+                                                        if (angf)
+                                                        {
+                                                                ball_set_angle(sprite, middleOfThree(0, ball_data->angle - 1, 2));
+                                                        }
+                                                        else
+                                                        {
+                                                                if (ball_data->angle == 2)
+                                                                {
+                                                                        ball_data->dx = -ball_data->dx;
+                                                                }
+                                                                else
+                                                                {
+                                                                        ball_set_angle(sprite, middleOfThree(0, ball_data->angle + 1, 2));
+                                                                }
+                                                        }
+						}
+                                        }
 				}
 
 				if (other_tag == BRICK) 
@@ -210,3 +274,33 @@ SpriteCollisionResponseType ball_collision_response(LCDSprite* sprite, LCDSprite
 {
 	return kCollisionTypeBounce;
 }
+
+void ball_set_angle(LCDSprite* sprite, float angle)
+{
+       if (sprite)
+       {
+               BallData* ball_data = (BallData*)get_playdate_API()->sprite->getUserdata(sprite);
+               if (ball_data)
+               {
+                       ball_data->angle = angle;
+                       
+                       if (angle == 2.f)
+                       {
+                                ball_data->dx = .5f * sign(ball_data->dx);
+                                ball_data->dy = 1.3f * sign(ball_data->dy);
+                       }
+                       else if (angle == 0.f)
+                       {
+                                ball_data->dx = 1.3f * sign(ball_data->dx);
+                                ball_data->dy = .5f * sign(ball_data->dy);
+                       }
+                       else
+		       {
+				ball_data->dx = 1.f * sign(ball_data->dx);
+                                ball_data->dy = 1.f * sign(ball_data->dy);
+                       }
+               }
+       }
+}
+                       {
+
