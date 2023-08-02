c9be1bfb1a48774e2ceb0d95730f7b3c5735a071
Ramo <ramo160689@gmail.com>
2023-08-02 16:20:46 +0200

    Update paddle.c

--- src/paddle.c
+++ src/paddle.c
@@ -9,14 +9,14 @@ struct PaddleData
 	float dx;
 };
 
-LCDSprite* paddle_create(float x, float y, LCDBitmap* image)
+LCDSprite* PADDLE_create(float x, float y, LCDBitmap* image)
 {
-	PlaydateAPI* pd = get_playdate_API();
+	PlaydateAPI* pd = getPlaydateAPI();
 
 	// Create new sprite
 	LCDSprite* paddle = pd->sprite->newSprite();
 
-	pd->sprite->setUpdateFunction(paddle, paddle_update_sprite);
+	pd->sprite->setUpdateFunction(paddle, PADDLE_updateSprite);
 	pd->sprite->setImage(paddle, image, kBitmapUnflipped);
 
 	int w, h;
@@ -25,7 +25,7 @@ LCDSprite* paddle_create(float x, float y, LCDBitmap* image)
 	// Create collision rect for paddle
 	PDRect cr = PDRectMake(0, 0, (float)w, (float)h);
 	pd->sprite->setCollideRect(paddle, cr);
-	pd->sprite->setCollisionResponseFunction(paddle, paddle_collision_response);
+	pd->sprite->setCollisionResponseFunction(paddle, PADDLE_collisionResponse);
 
 	pd->sprite->moveTo(paddle, x, y);
 	pd->sprite->setZIndex(paddle, 1000);
@@ -34,25 +34,26 @@ LCDSprite* paddle_create(float x, float y, LCDBitmap* image)
 	pd->sprite->setTag(paddle, PADDLE);
 
 	// Initialize paddle data
-	PaddleData* paddle_data = (PaddleData*)pd_malloc(sizeof(PaddleData));
+	PaddleData* paddle_data = pd_malloc(sizeof(PaddleData));
 	paddle_data->dx = 0.f;
 	pd->sprite->setUserdata(paddle, (void*)paddle_data);
 
 	return paddle;
 }
 
-void paddle_destroy(LCDSprite* sprite)
+void PADDLE_destroy(LCDSprite* sprite)
 {
-	PaddleData* paddle_data = (PaddleData*)get_playdate_API()->sprite->getUserdata(sprite);
+	getPlaydateAPI()->sprite->removeSprite(sprite);
+	PaddleData* paddle_data = (PaddleData*)getPlaydateAPI()->sprite->getUserdata(sprite);
 	pd_free(paddle_data);
 	pd_free(sprite);
 }
 
-void paddle_update_sprite(LCDSprite* sprite)
+void PADDLE_updateSprite(LCDSprite* sprite)
 {
 	if (sprite)
 	{
-		PlaydateAPI* pd = get_playdate_API();
+		PlaydateAPI* pd = getPlaydateAPI();
 		PaddleData* paddle_data = (PaddleData*)pd->sprite->getUserdata(sprite);
 		if (paddle_data)
 		{
@@ -75,10 +76,10 @@ void paddle_update_sprite(LCDSprite* sprite)
 				if (pd->sprite->getTag(collisions[0].other) == BALL) 
 				{
 					LCDSprite* ball = collisions[0].other;
-					if (ball_is_stuck(ball)) 
+					if (BALL_isStuck(ball)) 
 					{
 						//pd->sprite->moveBy(sprite, paddle_data->dx, 0.f);
-						pd->sprite->moveTo(sprite, middleOfThree((pad_rect.width / 2), pad_x + paddle_data->dx, (pd->display->getWidth() - (pad_rect.width / 2))), pad_y);
+						pd->sprite->moveTo(sprite, mid((pad_rect.width / 2), pad_x + paddle_data->dx, (pd->display->getWidth() - (pad_rect.width / 2))), pad_y);
 					}
 
 					if (collisions[0].normal.y = -1) 
@@ -92,47 +93,47 @@ void paddle_update_sprite(LCDSprite* sprite)
 					PDRect ball_rect = pd->sprite->getCollideRect(ball);
 					if (ball_y > pad_y - (pad_rect.height / 2) - 3) 
 					{
-						if (sign(ball_get_dx(ball)) == sign(paddle_get_dx(sprite)))
-                                                {
-                                                        ball_set_dx(ball, ball_get_dx(ball) + paddle_get_dx(sprite));
-                                                }
-                                                else
-                                                {
-                                                        ball_set_dx(ball, -ball_get_dx(ball));
-                                                        ball_set_dx(ball, ball_get_dx(ball) + paddle_get_dx(sprite));
-                                                }
- 
-                                                if (ball_x < pad_x)
-                                                {
-                                                        pd->sprite->moveTo(ball, pad_x - (pad_rect.width / 2), pad_y);
-                                                }
-                                                else
-                                                {
-                                                        pd->sprite->moveTo(ball, pad_x + (pad_rect.width / 2), pad_y);
-                                                }
+						if (sign(BALL_getDx(ball)) == sign(PADDLE_getDx(sprite)))
+						{
+							BALL_setDx(ball, BALL_getDx(ball) + PADDLE_getDx(sprite));
+						}
+						else
+						{
+							BALL_setDx(ball, -BALL_getDx(ball));
+							BALL_setDx(ball, BALL_getDx(ball) + PADDLE_getDx(sprite));
+						}
+
+						if (ball_x < pad_x)
+						{
+							pd->sprite->moveTo(ball, pad_x - (pad_rect.width / 2), pad_y);
+						}
+						else
+						{
+							pd->sprite->moveTo(ball, pad_x + (pad_rect.width / 2), pad_y);
+						}
 
 						if (!ball_rammed(ball))
-                                                {
-                                                        ball_set_rammed(ball, true);
-                                                }
+						{
+							BALL_setRammed(ball, true);
+						}
 					}
 				}
 			}
-			pd->sprite->moveTo(sprite, middleOfThree((pad_rect.width / 2), pad_x + paddle_data->dx, (pd->display->getWidth() - (pad_rect.width / 2))), pad_y);
+			pd->sprite->moveTo(sprite, mid((pad_rect.width / 2), pad_x + paddle_data->dx, (pd->display->getWidth() - (pad_rect.width / 2))), pad_y);
 		}
 	}
 }
 
-SpriteCollisionResponseType paddle_collision_response(LCDSprite* sprite, LCDSprite* other)
+SpriteCollisionResponseType PADDLE_collisionResponse(LCDSprite* sprite, LCDSprite* other)
 {
 	return kCollisionTypeOverlap;
 }
 
-void paddle_set_dx(LCDSprite* sprite, float value)
+void PADDLE_setDx(LCDSprite* sprite, float value)
 {
 	if (sprite)
 	{
-		PaddleData* paddle_data = (PaddleData*)get_playdate_API()->sprite->getUserdata(sprite);
+		PaddleData* paddle_data = (PaddleData*)getPlaydateAPI()->sprite->getUserdata(sprite);
 		if (paddle_data)
 		{
 			paddle_data->dx = value;
@@ -140,11 +141,11 @@ void paddle_set_dx(LCDSprite* sprite, float value)
 	}
 }
 
-float paddle_get_dx(LCDSprite* sprite)
+float PADDLE_getDx(LCDSprite* sprite)
 {
 	if(sprite)
 	{
-		PaddleData* paddle_data = (PaddleData*)get_playdate_API()->sprite->getUserdata(sprite);
+		PaddleData* paddle_data = (PaddleData*)getPlaydateAPI()->sprite->getUserdata(sprite);
 		if (paddle_data)
 		{
 			return paddle_data->dx;
