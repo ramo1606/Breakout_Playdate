11402d359339e1e9c01e0c1dbf2d19ee511da789
Ramo <ramo160689@gmail.com>
2023-08-02 16:08:09 +0200

    Update ball.c

--- src/ball.c
+++ src/ball.c
@@ -7,6 +7,11 @@
 
 #include <stdlib.h>
 
+static const float BALL_DX = 1.f;
+static const float BALL_DY = 1.f;
+static const float BALL_SPEED = 2.5f;
+static const float BALL_ANGLE = 1.f;
+
 struct BallData
 {
 	float dx;
@@ -18,13 +23,13 @@ struct BallData
 	bool rammed;
 };
 
-LCDSprite* ball_create(float x, float y, LCDBitmap* image)
+LCDSprite* BALL_create(float x, float y, LCDBitmap* image)
 {
-	PlaydateAPI* pd = get_playdate_API();
+	PlaydateAPI* pd = getPlaydateAPI();
 
 	LCDSprite* ball = pd->sprite->newSprite();
 
-	pd->sprite->setUpdateFunction(ball, ball_update_sprite);
+	pd->sprite->setUpdateFunction(ball, BALL_updateSprite);
 	pd->sprite->setImage(ball, image, kBitmapUnflipped);
 
 	int w, h;
@@ -33,7 +38,7 @@ LCDSprite* ball_create(float x, float y, LCDBitmap* image)
 	// Create collision rect for ball
 	PDRect cr = PDRectMake(0, 0, (float)w, (float)h);
 	pd->sprite->setCollideRect(ball, cr);
-	pd->sprite->setCollisionResponseFunction(ball, ball_collision_response);
+	pd->sprite->setCollisionResponseFunction(ball, BALL_collisionResponse);
 
 	pd->sprite->moveTo(ball, x, y);
 	pd->sprite->setZIndex(ball, 1000);
@@ -42,7 +47,7 @@ LCDSprite* ball_create(float x, float y, LCDBitmap* image)
 	pd->sprite->setTag(ball, BALL);
 
 	// Initialize paddle data
-	BallData* ball_data = (BallData*)pd_malloc(sizeof(BallData));
+	BallData* ball_data = pd_malloc(sizeof(BallData));
 	ball_data->dx = BALL_DX;
 	ball_data->dy = BALL_DY;
 	ball_data->speed = BALL_SPEED;
@@ -54,15 +59,15 @@ LCDSprite* ball_create(float x, float y, LCDBitmap* image)
 	return ball;
 }
 
-void ball_destroy(LCDSprite* sprite)
+void BALL_destroy(LCDSprite* sprite)
 {
-	get_playdate_API()->sprite->removeSprite(sprite);
-	BallData* ball_data = (BallData*)get_playdate_API()->sprite->getUserdata(sprite);
+	getPlaydateAPI()->sprite->removeSprite(sprite);
+	BallData* ball_data = (BallData*)getPlaydateAPI()->sprite->getUserdata(sprite);
 	pd_free(ball_data);
 	pd_free(sprite);
 }
 
-void ball_reflect(BallData* ball_data, CollisionVector normal)
+void BALL_reflect(BallData* ball_data, CollisionVector normal)
 {
 	if (normal.x != 0)
 	{
@@ -74,13 +79,13 @@ void ball_reflect(BallData* ball_data, CollisionVector normal)
 	}
 }
 
-void ball_update_sprite(LCDSprite* sprite)
+void BALL_updateSprite(LCDSprite* sprite)
 {
 	if (sprite)
 	{
-		PlaydateAPI* pd = get_playdate_API();
+		PlaydateAPI* pd = getPlaydateAPI();
 		BallData* ball_data = (BallData*)pd->sprite->getUserdata(sprite);
-		if (ball_data && !ball_is_dead(sprite) && !ball_is_stuck(sprite))
+		if (ball_data && !BALL_isDead(sprite) && !BALL_isStuck(sprite))
 		{
 			float x = 0;
 			float y = 0;
@@ -110,87 +115,87 @@ void ball_update_sprite(LCDSprite* sprite)
 					}
 					else 
 					{
-						ball_reflect(ball_data, collisions[0].normal);
+						BALL_reflect(ball_data, collisions[0].normal);
 					}
 				}
 
 				if (other_tag == PADDLE) 
 				{
 					float pad_x = 0;
-                                        float pad_y = 0;
-                                        pd->sprite->getPosition(collisions[0].other, &pad_x, &pad_y);
- 
-                                        bool bend = false;
-                                        bool angf = false;
-                                        if (collisions[0].normal.x != 0)
-                                        {
-                                                if (actual_y + ball_rect.width > pad_y + 3)
-                                                {
-                                                        ball_data->rammed = true;
-                                                }
-                                                else
-						{
-                                                        bend = true;
-                                                        angf = false;
-                                                        collisions[0].normal.y = -1;
-                                                        pd->sprite->moveTo(sprite, nextx, pad_y - ball_rect.height);
-                                                }
-                                        }
+					float pad_y = 0;
+					pd->sprite->getPosition(collisions[0].other, &pad_x, &pad_y);
 
-					ball_reflect(ball_data, collisions[0].normal);
+					bool bend = false;
+					bool angf = false;
+					if (collisions[0].normal.x != 0)
+					{
+						if (actual_y + ball_rect.width > pad_y + 3)
+						{
+							ball_data->rammed = true;
+						}
+						else
+						{
+							bend = true;
+							angf = false;
+							collisions[0].normal.y = -1;
+							pd->sprite->moveTo(sprite, nextx, pad_y - ball_rect.height);
+						}
+					}
+
+					BALL_reflect(ball_data, collisions[0].normal);
 
 					if (collisions[0].normal.y == -1)
-                                        {
-                                                if (!bend && abs(paddle_get_dx(collisions[0].other)) > 1)
-                                                {
-                                                        bend = true;
-                                                        if (sign(paddle_get_dx(collisions[0].other)) == sign(ball_get_dx(sprite)))
-                                                        {
-                                                                angf = true;
-                                                        }
-                                                        else
-                                                        {
-                                                                angf = false;
-                                                        }
-                                                }
+					{
+						if (!bend && abs(PADDLE_getDx(collisions[0].other)) > 1)
+						{
+							bend = true;
+							if (sign(PADDLE_getDx(collisions[0].other)) == sign(BALL_getDx(sprite)))
+							{
+								angf = true;
+							}
+							else
+							{
+								angf = false;
+							}
+						}
 						
 						if (bend)
-                                                {
-                                                        if (angf)
-                                                        {
-                                                                ball_set_angle(sprite, middleOfThree(0, ball_data->angle - 1, 2));
-                                                        }
-                                                        else
-                                                        {
-                                                                if (ball_data->angle == 2)
-                                                                {
-                                                                        ball_data->dx = -ball_data->dx;
-                                                                }
-                                                                else
-                                                                {
-                                                                        ball_set_angle(sprite, middleOfThree(0, ball_data->angle + 1, 2));
-                                                                }
-                                                        }
+                        {
+							if (angf)
+							{
+								BALL_setAngle(sprite, mid(0, ball_data->angle - 1, 2));
+							}
+							else
+							{
+								if (ball_data->angle == 2)
+								{
+									ball_data->dx = -ball_data->dx;
+								}
+								else
+								{
+									BALL_setAngle(sprite, mid(0, ball_data->angle + 1, 2));
+								}
+							}
 						}
-                                        }
+                    }
 				}
 
 				if (other_tag == BRICK) 
 				{
 					pd->sprite->removeSprite(collisions[0].other);
-					brick_destroy(collisions[0].other);
-					ball_reflect(ball_data, collisions[0].normal);
+					BRICK_destroy(collisions[0].other);
+					BALL_reflect(ball_data, collisions[0].normal);
 				}
 			}
 		}
 	}
 }
 
-void ball_set_dx(LCDSprite* sprite, float value)
+void BALL_setDx(LCDSprite* sprite, float value)
 {
 	if (sprite)
 	{
-		BallData* ball_data = (BallData*)get_playdate_API()->sprite->getUserdata(sprite);
+		BallData* ball_data = (BallData*)getPlaydateAPI()->sprite->getUserdata(sprite);
 		if (ball_data)
 		{
 			ball_data->dx = value;
@@ -198,23 +203,24 @@ void ball_set_dx(LCDSprite* sprite, float value)
 	}
 }
 
-float ball_get_dx(LCDSprite* sprite)
+float BALL_getDx(LCDSprite* sprite)
 {
 	if (sprite)
 	{
-		BallData* ball_data = (BallData*)get_playdate_API()->sprite->getUserdata(sprite);
+		BallData* ball_data = (BallData*)getPlaydateAPI()->sprite->getUserdata(sprite);
 		if (ball_data)
 		{
 			return ball_data->dx;
 		}
 	}
+	return 0;
 }
 
-void ball_set_dy(LCDSprite* sprite, float value)
+void BALL_setDy(LCDSprite* sprite, float value)
 {
 	if (sprite)
 	{
-		BallData* ball_data = (BallData*)get_playdate_API()->sprite->getUserdata(sprite);
+		BallData* ball_data = (BallData*)getPlaydateAPI()->sprite->getUserdata(sprite);
 		if (ball_data)
 		{
 			ball_data->dy = value;
@@ -222,23 +228,24 @@ void ball_set_dy(LCDSprite* sprite, float value)
 	}
 }
 
-float ball_get_dy(LCDSprite* sprite)
+float BALL_getDy(LCDSprite* sprite)
 {
 	if (sprite)
 	{
-		BallData* ball_data = (BallData*)get_playdate_API()->sprite->getUserdata(sprite);
+		BallData* ball_data = (BallData*)getPlaydateAPI()->sprite->getUserdata(sprite);
 		if (ball_data)
 		{
 			return ball_data->dy;
 		}
 	}
+	return 0;
 }
 
-void ball_set_stuck(LCDSprite* sprite, bool stuck)
+void BALL_setStuck(LCDSprite* sprite, bool stuck)
 {
 	if (sprite)
 	{
-		BallData* ball_data = (BallData*)get_playdate_API()->sprite->getUserdata(sprite);
+		BallData* ball_data = (BallData*)getPlaydateAPI()->sprite->getUserdata(sprite);
 		if (ball_data)
 		{
 			ball_data->stuck = stuck;
@@ -246,81 +253,84 @@ void ball_set_stuck(LCDSprite* sprite, bool stuck)
 	}
 }
 
-bool ball_is_stuck(LCDSprite* sprite)
+bool BALL_isStuck(LCDSprite* sprite)
 {
 	if (sprite)
 	{
-		BallData* ball_data = (BallData*)get_playdate_API()->sprite->getUserdata(sprite);
+		BallData* ball_data = (BallData*)getPlaydateAPI()->sprite->getUserdata(sprite);
 		if (ball_data)
 		{
 			return ball_data->stuck;
 		}
 	}
+	return false;
 }
 
-bool ball_is_dead(LCDSprite* sprite)
+bool BALL_isDead(LCDSprite* sprite)
 {
 	if (sprite)
 	{
-		BallData* ball_data = (BallData*)get_playdate_API()->sprite->getUserdata(sprite);
+		BallData* ball_data = (BallData*)getPlaydateAPI()->sprite->getUserdata(sprite);
 		if (ball_data)
 		{
 			return ball_data->dead;
 		}
 	}
+	return false;
 }
 
-SpriteCollisionResponseType ball_collision_response(LCDSprite* sprite, LCDSprite* other)
+SpriteCollisionResponseType BALL_collisionResponse(LCDSprite* sprite, LCDSprite* other)
 {
 	return kCollisionTypeBounce;
 }
 
-void ball_set_angle(LCDSprite* sprite, float angle)
+void BALL_setAngle(LCDSprite* sprite, float angle)
 {
 	if (sprite)
-        {
-                BallData* ball_data = (BallData*)get_playdate_API()->sprite->getUserdata(sprite);
-                if (ball_data)
-                {
-                        ball_data->angle = angle;
-                        if (angle == 2.f)
-                        {
-                                ball_data->dx = .5f * sign(ball_data->dx);
-                                ball_data->dy = 1.3f * sign(ball_data->dy);
-                        }
-                        else if (angle == 0.f)
-                        {
-                                ball_data->dx = 1.3f * sign(ball_data->dx);
-                                ball_data->dy = .5f * sign(ball_data->dy);
-                        }
-                        else
-                        {
+    {
+		BallData* ball_data = (BallData*)getPlaydateAPI()->sprite->getUserdata(sprite);
+		if (ball_data)
+		{
+			ball_data->angle = angle;
+			if (angle == 2.f)
+			{
+				ball_data->dx = .5f * sign(ball_data->dx);
+				ball_data->dy = 1.3f * sign(ball_data->dy);
+			}
+			else if (angle == 0.f)
+			{
+				ball_data->dx = 1.3f * sign(ball_data->dx);
+				ball_data->dy = .5f * sign(ball_data->dy);
+			}
+			else
+			{
 				ball_data->dx = 1.f * sign(ball_data->dx);
 				ball_data->dy = 1.f * sign(ball_data->dy);
-                        }
-                }
-        }
+			}
+		}
+    }
 }
 
-bool ball_rammed(LCDSprite* sprite)
+bool BALL_rammed(LCDSprite* sprite)
 {
 	if (sprite)
-        {
-		BallData* ball_data = (BallData*)get_playdate_API()->sprite->getUserdata(sprite);
-                if (ball_data)
-                {
+    {
+		BallData* ball_data = (BallData*)getPlaydateAPI()->sprite->getUserdata(sprite);
+		if (ball_data)
+		{
 			return ball_data->rammed;
-                }
         }
+    }
+	return false;
 }
 
-void ball_set_rammed(LCDSprite* sprite, bool rammed)
+void BALL_setRammed(LCDSprite* sprite, bool rammed)
 {
 	if (sprite)
 	{
-		BallData* ball_data = (BallData*)get_playdate_API()->sprite->getUserdata(sprite);
-                if (ball_data)
-                {
+		BallData* ball_data = (BallData*)getPlaydateAPI()->sprite->getUserdata(sprite);
+        if (ball_data)
+        {
 			ball_data->rammed = rammed;
 		}
 	}
