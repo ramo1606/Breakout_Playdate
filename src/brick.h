8183c05cc67a5912fa5d6d784698e6198e1350c7
Ramo <ramo160689@gmail.com>
2023-08-02 17:35:29 +0200

    Update brick.h

--- src/brick.h
+++ src/brick.h
@@ -2,10 +2,11 @@
 #define BRICK_H
 
 #include "pd_api.h"
+#include "common.h"
 
 typedef struct BrickData BrickData;
 
-LCDSprite* BRICK_create(float x, float y, LCDBitmap* image);
+LCDSprite* BRICK_create(float x, float y, LCDBitmap* image, EBrickType type);
 void BRICK_destroy(LCDSprite* sprite);
 void BRICK_updateSprite(LCDSprite* sprite);
 
@@ -13,6 +14,9 @@ void BRICK_setDx(LCDSprite* sprite, float value);
 float BRICK_getDx(LCDSprite* sprite);
 void BRICK_setDy(LCDSprite* sprite, float value);
 float BRICK_getDy(LCDSprite* sprite);
+EBrickType BRICK_getType(LCDSprite* sprite);
+
+EBrickType BRICK_translateType(char type);
 
 SpriteCollisionResponseType BRICK_collisionResponse(LCDSprite* sprite, LCDSprite* other);
 
