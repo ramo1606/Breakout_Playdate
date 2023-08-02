87dedcc36f8f8ded4d7f78642e9aae71293b815d
Ramo <ramo160689@gmail.com>
2023-08-02 16:24:29 +0200

    Update utils.h

--- src/utils.h
+++ src/utils.h
@@ -1,12 +1,19 @@
-#pragma once
+#ifndef UTILS_H
+#define UTILS_H
+
 #include "pd_api.h"
+#include "common.h"
+
 #include <stdbool.h>
 #include <math.h>
 
 #define EPSILON 0.0001
 #define sign(x)  (signbit(x) ?  -1 : 1)
 
-bool AreSame(float a, float b);
+/* Compares two float numbers */
+bool areEqual(float a, float b);
 
-// Function to find the middle of three number
-float middleOfThree(float a, float b, float c);
+/* Function to find the middle of three number */
+float mid(float a, float b, float c);
+
+#endif
