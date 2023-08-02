4f8d275100128c118ebc8148edbf7573cd49dab8
Ramo <ramo160689@gmail.com>
2023-08-02 15:59:24 +0200

    Update and rename game.h to engine.h

---
+++ src/engine.h
@@ -0,0 +1,9 @@
+#ifndef ENGINE_H
+#define ENGINE_H
+
+#include "pd_api.h"
+
+void ENGINE_create(PlaydateAPI* p);
+int ENGINE_update(void);
+
+#endif
--- src/game.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "pd_api.h"
-#include <stdbool.h>
-
-void create_game(PlaydateAPI* p);
-
-int update();
