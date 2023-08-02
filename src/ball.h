9d715a590e88a1a97d5145069c01d063309caec8
Ramo <ramo160689@gmail.com>
2023-08-02 16:07:53 +0200

    Update ball.h

--- src/ball.h
+++ src/ball.h
@@ -1,23 +1,26 @@
-#pragma once
+#ifndef BALL_H
+#define BALL_H
+
 #include "pd_api.h"
 #include <stdbool.h>
 
 typedef struct BallData BallData;
 
-LCDSprite* ball_create(float x, float y, LCDBitmap* image);
-void ball_destroy(LCDSprite* sprite);
-void ball_update_sprite(LCDSprite* sprite);
+LCDSprite* BALL_create(float x, float y, LCDBitmap* image);
+void BALL_destroy(LCDSprite* sprite);
+void BALL_updateSprite(LCDSprite* sprite);
 
-void ball_set_dx(LCDSprite* sprite, float value);
-float ball_get_dx(LCDSprite* sprite);
-void ball_set_dy(LCDSprite* sprite, float value);
-float ball_get_dy(LCDSprite* sprite);
-void ball_set_stuck(LCDSprite* sprite, bool stuck);
-bool ball_is_stuck(LCDSprite* sprite);
-bool ball_is_dead(LCDSprite* sprite);
-void ball_set_angle(LCDSprite* sprite, float angle);
-bool ball_rammed(LCDSprite* sprite);
-void ball_set_rammed(LCDSprite* sprite, bool rammed);
+void BALL_setDx(LCDSprite* sprite, float value);
+float BALL_getDx(LCDSprite* sprite);
+void BALL_setDy(LCDSprite* sprite, float value);
+float BALL_getDy(LCDSprite* sprite);
+void BALL_setStuck(LCDSprite* sprite, bool stuck);
+bool BALL_isStuck(LCDSprite* sprite);
+bool BALL_isDead(LCDSprite* sprite);
+void BALL_setAngle(LCDSprite* sprite, float angle);
+bool BALL_rammed(LCDSprite* sprite);
+void BALL_setRammed(LCDSprite* sprite, bool rammed);
 
+SpriteCollisionResponseType BALL_collisionResponse(LCDSprite* sprite, LCDSprite* other);
 
-SpriteCollisionResponseType ball_collision_response(LCDSprite* sprite, LCDSprite* other);
+#endif
