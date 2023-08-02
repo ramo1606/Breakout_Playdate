b2fa438f0ed4200be0cc143fc76bc477c7c35e05
Ramo <ramo160689@gmail.com>
2023-08-02 16:19:45 +0200

    Update memory.h

--- src/memory.h
+++ src/memory.h
@@ -1,10 +1,13 @@
-#pragma once
+#ifndef MEMORY_H
+#define MEMORY_H
 
 #include "pd_api.h"
 
-void initializeMemoryTools(PlaydateAPI* playdate);
+void MEMORY_initializeMemoryTools(PlaydateAPI* playdate);
 
 void* pd_malloc(size_t size);
 void* pd_calloc(size_t count, size_t size);
 void* pd_realloc(void* ptr, size_t size);
 void  pd_free(void* ptr);
+
+#endif
