935bf2072376f0ac3aa169e4cf586f9f27b02965
Ramo <ramo160689@gmail.com>
2023-08-03 16:55:34 +0200

    Update state.h

--- src/state.h
+++ src/state.h
@@ -6,9 +6,11 @@
 typedef unsigned int (*fnPtr)(void);
 typedef unsigned int (*fnPtrFl)(float);
 typedef EMode (*fnPtrEMode)(void);
+typedef void (*fnPtrSetMode)(EMode);
 
 typedef struct
 {
+    fnPtrSetMode setNextMode;
     fnPtrEMode getNextState;
     fnPtr init;
     fnPtrFl update;
